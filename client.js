126480a3986410962888e36fd557e168dc0bcbe9
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-04-08 20:36:17 -0500

    server can take requests and emit events now

--- client.js
+++ client.js
@@ -1,9 +1,28 @@
 var socket = require('socket.io-client')('http://localhost:3000');
+var config = require('./config.json');
+
 
 socket.on('connect', function() {
+
     console.log('connected');
+
+    socket.emit('validateSession', config.PASSWORD);
+
 });
 
+
+socket.on('mouseEvent', function(data) {
+    console.log(data);
+});
+
+
+socket.on('randomEvent', function(data) {
+    console.log(data);
+});
+
+
 socket.on('disconnect', function() {
+
     console.log('disconnected');
+
 });
--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
+var config = require('./config.json');
+
 
 server.listen(3000, function() {
     console.log('Server listening at port %d', 3000);
@@ -10,6 +12,26 @@ server.listen(3000, function() {
 
 
 io.on('connection', function(socket) {
-    console.log('new connection from ' +  socket.handshake.address)
+    console.log('new connection from ' + socket.handshake.address);
+
+    valid_connection = false;
+
+    app.get('/', function(req, res) {
+        socket.emit('randomEvent', 'random');
+        res.end('success');
+    });
+
+    socket.on('validateSession', function(data) {
+        console.log('validating');
+        console.log(data);
+        console.log(config.PASSWORD);
+        if (config.PASSWORD === data) {
+            console.log('validated');
+            valid_connection = true;
+        }
+        else {
+            console.log('rejected');
+        }
+    });
 
 });
