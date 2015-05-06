1e67d61eb4d332d4bf0a886b14d5ae1d1df00e64
Jason Xiong <jason.j.xiong@vanderbilt.edu>
2015-05-06 17:14:25 -0400

    added input thread

--- server/windows/windows/main.cpp
+++ server/windows/windows/main.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
+#include <thread>
+#include <string>
 #include <Windows.h>
+
+bool QUIT = false;
+
+void process_input() {
+    std::string str_input;
+    while (!QUIT) {
+        std::getline(std::cin, str_input);
+        if (str_input == "quit") {
+            QUIT = true;
+        }
+    }
+}
 
 int main() {
 
+    std::thread input_thread(process_input);
+
     POINT newPoint;
     POINT oldPoint = { 0, 0 };
-    for (size_t i = 0; i < 10; i++) {
+    while (!QUIT) {
         if (GetCursorPos(&newPoint) && newPoint.x != oldPoint.x && newPoint.y != oldPoint.y) {
             std::cout << newPoint.x << ":" << newPoint.y << std::endl;
             oldPoint = newPoint;
         }
-        Sleep(300);
     }
 
     return 0;
