044afb6039b3c712c70aa677592ca0997cf9c840
Jason Xiong <jason.j.xiong@vanderbilt.edu>
2015-05-06 15:48:06 -0400

    added gitignore

--- server/windows/windows/main.cpp
+++ server/windows/windows/main.cpp
@@ -3,9 +3,14 @@
 
 int main() {
 
-    POINT p;
-    if (GetCursorPos(&p)) {
-        std::cout << p.x << ":" << p.y << std::endl;
+    POINT newPoint;
+    POINT oldPoint = { 0, 0 };
+    for (size_t i = 0; i < 10; i++) {
+        if (GetCursorPos(&newPoint) && newPoint.x != oldPoint.x && newPoint.y != oldPoint.y) {
+            std::cout << newPoint.x << ":" << newPoint.y << std::endl;
+            oldPoint = newPoint;
+        }
+        Sleep(300);
     }
 
     return 0;
