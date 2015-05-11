7fb077da68d85f38cca3a62be8c5ede237aaa9a6
Jason Xiong <jason.j.xiong@vanderbilt.edu>
2015-05-10 20:28:52 -0400

    fixed old build, debug build rule added

--- server/buildrules/buildwindowsdebug.bat
+++ server/buildrules/buildwindowsdebug.bat
@@ -1,7 +1,5 @@
 msbuild ..\windows\eventlisteners\windows\windows.vcxproj /p:configuration=release /p:DebugSymbols=false /p:Debugtype=None
 msbuild ..\windows\MoveMouse\MoveMouse\MoveMouse.vcxproj /p:configuration=release /p:DebugSymbols=false /p:Debugtype=None
 mkdir ..\releasesrc
-move ..\windows\eventlisteners\windows\release\EventListener.exe ..\releasesrc
-move ..\windows\MoveMouse\MoveMouse\release\MoveMouse.exe ..\releasesrc
-rm ..\windows\eventlisteners\windows\release /s
-rm ..\windows\MoveMouse\MoveMouse\release /s
+copy ..\windows\eventlisteners\windows\release\EventListener.exe ..\releasesrc
+copy ..\windows\MoveMouse\MoveMouse\release\MoveMouse.exe ..\releasesrc
