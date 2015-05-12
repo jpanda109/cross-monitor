71813ef160c9bf125fbb3133f394d005f1b3d47a
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-05-12 19:36:22 -0400

    added build rule

--- server/buildrules/buildlinux.sh
+++ server/buildrules/buildlinux.sh
@@ -1,4 +1,7 @@
+mkdir -p ../releasesrc
 cd ../linux/EventListener
 g++ main.cpp -o EventListener.out -lpthread -lX11 -std=c++11
-mkdir -p ../../releasesrc
 cp EventListener.out ../../releasesrc
+cd ../MoveMouse
+g++ main.cpp -o MoveMouse.out -lX11 -std=c++11
+cp MoveMouse.out ../../releasesrc
