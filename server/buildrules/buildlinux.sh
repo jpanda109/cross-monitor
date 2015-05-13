c9fec584625fef645dee7a66f61e29586766c44b
jason.j.xiong@vanderbilt.edu <jason.j.xiong@gmail.com>
2015-05-12 21:23:29 -0400

    fixed build rule

--- server/buildrules/buildlinux.sh
+++ server/buildrules/buildlinux.sh
@@ -1,7 +1,7 @@
 mkdir -p ../releasesrc
 cd ../linux/EventListener
-g++ main.cpp -o EventListener.out -lpthread -lX11 -std=c++11
-cp EventListener.out ../../releasesrc
+g++ main.cpp -o EventListener.out -pthread -lX11 -std=c++11
+cp EventListener.out ../../releasesrc/EventListener
 cd ../MoveMouse
 g++ main.cpp -o MoveMouse.out -lX11 -std=c++11
-cp MoveMouse.out ../../releasesrc
+cp MoveMouse.out ../../releasesrc/MoveMouse
